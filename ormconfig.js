8e78c39c0ab5d671ce3b3fe44d8f7363c93de485
Jakub Filinger <kubafilinger@gmail.com>
2021-03-15 21:01:29 +0100

    Change process env to getenv

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const getenv = require('getenv');
+
 module.exports = {
   type: 'mysql',
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: getenv('DATABASE_HOST'),
+  port: getenv('DATABASE_PORT'),
+  username: getenv('DATABASE_USERNAME'),
+  password: getenv('DATABASE_PASSWORD'),
+  database: getenv('DATABASE_NAME'),
   logging: true,
   migrations: ['src/infrastructure/migrations/*.ts'],
   entities: ['src/domain/**/*.entity.ts'],
